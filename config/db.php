7dac62ad714b374c09b0fddd2a40d8ae651d32de
akhils <akhil@webyfy.com>
2024-08-12 11:19:59 +0530

    fixed

--- config/db.php
+++ config/db.php
@@ -1,3 +1,4 @@
+
 <?php
 $servername = "localhost";  // or your database server name
 $username = "root";         // your database username
@@ -11,9 +12,8 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-echo "Connected successfully";
+// echo "Connected successfully";
 
 // Close connection
-$conn->close();
+// $conn->close();
 ?>
