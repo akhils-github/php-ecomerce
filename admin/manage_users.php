0722f9c8a864a43493c450b31ef1b16c82a1b85c
akhils <akhil@webyfy.com>
2024-08-12 13:57:36 +0530

    fixed

--- admin/manage_users.php
+++ admin/manage_users.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['admin'])) {
 
 
 // $sql = "SELECT * FROM users";
-$sql = "SELECT id, username FROM users WHERE role != 'a'";
+$sql = "SELECT id, username FROM users WHERE role != 'admin'";
 $result = $conn->query($sql);
 ?>
 <div class="p-5">
@@ -22,116 +22,36 @@ $result = $conn->query($sql);
 <h5 class="card-title h3">User Details</h5>
 
 </div>
-<table class="table align-middle mb-0 bg-white">
+<table class="table align-middle mb-0 bg-white w-75">
   <thead class="bg-light">
     <tr>
-      <th>Name</th>
-      <th>Title</th>
-      <th>Status</th>
-      <th>Position</th>
-      <th>Actions</th>
+      <th>Id</th>
+      <th>Username</th>
+        <th>Actions</th>
     </tr>
   </thead>
   <tbody>
   <?php while ($row = $result->fetch_assoc()) { ?>
     <tr>
       <td>
-        <div class="d-flex align-items-center">
-          <!-- <img
-              src="https://mdbootstrap.com/img/new/avatars/8.jpg"
-              alt=""
-              style="width: 45px; height: 45px"
-              class="rounded-circle"
-              /> -->
-          <div class="ms-3">
-            <p class="fw-bold mb-1"><?php echo $row['id']; ?></p>
-            <!-- <p class="text-muted mb-0">john.doe@gmail.com</p> -->
-          </div>
-        </div>
+
+       <?php echo $row['id']; ?>
       </td>
       <td>
         <p class="fw-normal mb-1"><?php echo $row['username']; ?></p>
-        <p class="text-muted mb-0">IT department</p>
       </td>
+   
       <td>
-        <span class="badge badge-success rounded-pill d-inline">Active</span>
-      </td>
-      <td>Senior</td>
-      <td>
-        <button type="button" class="btn btn-link btn-sm btn-rounded">
+        <button type="button" class="btn btn-warning btn-sm btn-rounded">
           Edit
+        </button>
+        <button type="button" class="btn btn-danger btn-sm btn-rounded">
+          Delete
         </button>
       </td>
     </tr>
     <?php } ?>
-    <tr>
-      <td>
-        <div class="d-flex align-items-center">
-          <img
-              src="https://mdbootstrap.com/img/new/avatars/6.jpg"
-              class="rounded-circle"
-              alt=""
-              style="width: 45px; height: 45px"
-              />
-          <div class="ms-3">
-            <p class="fw-bold mb-1">Alex Ray</p>
-            <p class="text-muted mb-0">alex.ray@gmail.com</p>
-          </div>
-        </div>
-      </td>
-      <td>
-        <p class="fw-normal mb-1">Consultant</p>
-        <p class="text-muted mb-0">Finance</p>
-      </td>
-      <td>
-        <span class="badge badge-primary rounded-pill d-inline"
-              >Onboarding</span
-          >
-      </td>
-      <td>Junior</td>
-      <td>
-        <button
-                type="button"
-                class="btn btn-link btn-rounded btn-sm fw-bold"
-                data-mdb-ripple-color="dark"
-                >
-          Edit
-        </button>
-      </td>
-    </tr>
-    <tr>
-      <td>
-        <div class="d-flex align-items-center">
-          <img
-              src="https://mdbootstrap.com/img/new/avatars/7.jpg"
-              class="rounded-circle"
-              alt=""
-              style="width: 45px; height: 45px"
-              />
-          <div class="ms-3">
-            <p class="fw-bold mb-1">Kate Hunington</p>
-            <p class="text-muted mb-0">kate.hunington@gmail.com</p>
-          </div>
-        </div>
-      </td>
-      <td>
-        <p class="fw-normal mb-1">Designer</p>
-        <p class="text-muted mb-0">UI/UX</p>
-      </td>
-      <td>
-        <span class="badge badge-warning rounded-pill d-inline">Awaiting</span>
-      </td>
-      <td>Senior</td>
-      <td>
-        <button
-                type="button"
-                class="btn btn-link btn-rounded btn-sm fw-bold"
-                data-mdb-ripple-color="dark"
-                >
-          Edit
-        </button>
-      </td>
-    </tr>
+ 
   </tbody>
 </table>
 </div>
