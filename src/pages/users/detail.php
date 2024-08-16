1c2c6fc499f49c8f1f5ff304b0fe9da47bbcad2b
akhils <akhil@webyfy.com>
2024-08-16 19:50:52 +0530

    Setuped Projects

---
+++ src/pages/users/detail.php
@@ -0,0 +1,71 @@
+
+<?php
+include('../../index.php'); 
+?>
+
+<section class="section section-divider gray about" id="about">
+        <div class="container">
+
+          <div class="about-banner">
+            <img src="../../../public/assets/images/about-banner.png" width="509" height="459" loading="lazy" alt="Burger with Drinks"
+              class="w-100 about-img">
+
+            <img src="../../../public/assets/images/sale-shape-red.png" width="216" height="226" alt="get up to 50% off now"
+              class="abs-img scale-up-anim">
+          </div>
+
+          <div class="about-content">
+
+            <h2 class="h2 section-title">
+              Caferio, Burgers, and Best Pizzas
+              <span class="span">in Town!</span>
+            </h2>
+
+            <p class="section-text">
+              The restaurants in Hangzhou also catered to many northern Chinese who had fled south from Kaifeng during
+              the Jurchen
+              invasion of the 1120s, while it is also known that many restaurants were run by families.
+            </p>
+
+            <ul class="about-list">
+
+              <li class="about-item">
+                <ion-icon name="checkmark-outline"></ion-icon>
+
+                <span class="span">Delicious & Healthy Foods</span>
+              </li>
+
+              <li class="about-item">
+                <ion-icon name="checkmark-outline"></ion-icon>
+
+                <span class="span">Spacific Family And Kids Zone</span>
+              </li>
+
+              <li class="about-item">
+                <ion-icon name="checkmark-outline"></ion-icon>
+
+                <span class="span">Music & Other Facilities</span>
+              </li>
+
+              <li class="about-item">
+                <ion-icon name="checkmark-outline"></ion-icon>
+
+                <span class="span">Fastest Food Home Delivery</span>
+              </li>
+
+            </ul>
+                <div class="col-md-4 mb-3">
+                    <div class="qty-container">
+                        <button class="qty-btn-minus btn-primary btn-rounded mr-1" type="button">-</button>
+                        <input type="text" name="qty" value="0" class="input-qty input-rounded"/>
+                        <button class="qty-btn-plus btn-primary btn-rounded ml-1" type="button">+</button>
+                    </div>
+                </div>
+            
+           
+            <button class="btn btn-hover">Order Now</button>
+
+          </div>
+
+        </div>
+      </section>
